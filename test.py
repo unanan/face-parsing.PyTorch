f01e0aff3dd099b35564ed1197cadf521a0b37d7
unaG <veraguo11@163.com>
2021-07-09 19:08:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -68,7 +68,7 @@ def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth')
         for image_path in os.listdir(dspth):
             img = Image.open(osp.join(dspth, image_path))
             image = img.resize((512, 512), Image.BILINEAR)
-            print(image.shape)
+            print(image.size())
             img = to_tensor(image)
             img = torch.unsqueeze(img, 0)
             img = img.to(torch.device("cuda:0"))
