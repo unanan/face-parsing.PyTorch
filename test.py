913c03a2f0a11db9d05c99ce65a67440b249c419
unaG <veraguo11@163.com>
2021-07-09 19:06:19 +0800

    Update test.py

--- test.py
+++ test.py
@@ -55,7 +55,7 @@ def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth')
 
     n_classes = 19
     net = BiSeNet(n_classes=n_classes)
-    net.to(torch.device("cuda:1"))
+    net.to(torch.device("cuda:0"))
     save_pth = osp.join('res/cp', cp)
     net.load_state_dict(torch.load(save_pth))
     net.eval()
@@ -70,7 +70,7 @@ def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth')
             image = img.resize((512, 512), Image.BILINEAR)
             img = to_tensor(image)
             img = torch.unsqueeze(img, 0)
-            img = img.to(torch.device("cuda:1"))
+            img = img.to(torch.device("cuda:0"))
             out = net(img)[0]
             parsing = out.squeeze(0).cpu().numpy().argmax(0)
             # print(parsing)
